2405cfc14b8e50089cc1c24fb0886b598d00903e
vishal singh <join4m.com@gmail.com>
2015-10-04 14:30:59 +0530

    created model for flipkart mobile section

--- kamkimat/scrapper/models.py
+++ kamkimat/scrapper/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+#Flipkart mobile category
+class FlipkartMobile(models.Model):
+	product_name = models.CharField(max_length=200)
+	product_sub_name = models.CharField(max_length=200)
+	product_u_id = models.CharField(max_length=200, unique=True)
+	product_m_id = models.CharField(max_length=200, blank=True)
+	product_availability = models.BooleanField()
+	product_price = models.CharField(max_length=200)
+	product_desc = models.CharField(max_length=1000, blank=True)
+	product_specifications= models.CharField(max_length=10000, blank=True)
+	product_url = models.CharField(max_length=1000)
+	product_scrape_time = models.DateTimeField('Last Update time')
+	product_img_url = models.CharField(max_length=1000)
