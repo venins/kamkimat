62a479a8f2dfc0a74e25716f2d4d36aa70e921d2
vishal singh <join4m.com@gmail.com>
2015-10-04 12:10:36 +0530

    added database and timezone details

--- kamkimat/kamkimat/settings.py
+++ kamkimat/kamkimat/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'kamkimat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kamkimat',
+        'USER': 'kamkimat',
+        'PASSWORD': 'kamkimat',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -87,7 +91,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
