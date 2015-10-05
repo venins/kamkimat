4c7a0c367e804c268929fb378936a527876c0d42
vishal singh <join4m.com@gmail.com>
2015-10-05 18:53:50 +0530

    fix snapdealmobile import issue

--- kamkimat/scrapper/admin.py
+++ kamkimat/scrapper/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import FlipkartMobile
+from .models import FlipkartMobile, SnapdealMobile
 
 admin.site.register(FlipkartMobile)
 admin.site.register(SnapdealMobile)
